ed053bb768b206abb38d3011ea4f5dfd59ac8146
Cristian Marcel <lcristianiim@yahoo.com>
2015-06-19 22:23:53 +0300

    Handle requests adjustment in displaying 'Cannot read directory'. Comments added.

--- modules/handleReguests.js
+++ modules/handleReguests.js
@@ -2,14 +2,22 @@ var fs = require('fs'),
     handleMimeType = require('./handleMimeType');
 
 module.exports = function(req, res) {
+    // Check if link exists on disk
     if (fs.existsSync('public' + req.url)) {
+        // Checking if public/url is directory
         if (fs.lstatSync('public' + req.url).isDirectory()) {
-            if ('public' + req.url + 'index.html') {
+            // render index.html if it exists in directory
+            if (fs.existsSync('public' + req.url + 'index.html')) {
                 res.setHeader('Content-Type', 'html');
                 res.end(fs.readFileSync('public' + req.url + 'index.html'));
-            }
-            res.end('Can not read directory');
+            } else {
+                // display 'Can not read directory' if no index.html found in directory
+                res.end('Can not read directory');
+            };
         } else {
+            // if the url is not a directory get the file in fileContent
+            // getting the header for the file
+            // display file
             var fileContent = fs.readFileSync('public' + req.url);
             res.setHeader("Content-Type", handleMimeType('public' + req.url));
             res.end(fileContent);
